1501df66a63dc182f0ffda132152f7a339f7b7d1
Pradnyawani <pradnyawani7@gmail.com>
2025-11-04 11:45:23 +0530

    Add files via upload

---
+++ Assignments/Assignment_3/program_4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+void Displayconvert(char CValue)
+{
+    if(CValue>='A' && CValue<='Z')
+    {
+        printf("%c",CValue+32);
+    }
+    else if(CValue>='a' && CValue<='z')
+    {
+        printf("%c",CValue-32);
+    }
+}
+
+
+int main()
+{
+    char cValue='\0';
+    printf("Enter Character\n");
+    scanf("%c",&cValue);
+    Displayconvert(cValue);
+
+    return 0;
+}
