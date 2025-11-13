5a1812b3eb62fdc968825785e54c515ff8badef8
Pradnyawani <pradnyawani7@gmail.com>
2025-11-13 14:36:59 +0530

    Add files via upload

---
+++ Assignments/Assignment_13/program_1.c
@@ -0,0 +1,39 @@
+
+
+#include<stdio.h>
+//////////////////////////////////////////////////////////////
+// Function Name : print_numbers
+// Description   : Accept number from user and print all numbers 
+//                 from 1 to that number.
+// Input         : Integer
+// Output        : Displays numbers from 1 to N
+// Author        :Pradnya Bhausaheb Wani
+// Date          : 13/11/2025
+///////////////////////////////////////////////////////////////
+
+void print_numbers(int limit)
+{
+    int iNo = 0;
+
+    if(limit <= 0)
+    {
+        printf("Enter number:");
+    }
+    for(iNo = 1; iNo <= limit; iNo++)
+    {
+        printf("%d", iNo);
+    }
+
+}
+int main()
+{
+    int  limit = 0;
+
+    printf("Enter Number:");
+    scanf("%d",&limit);
+
+    print_numbers(limit);
+
+    return 0;
+}
+// Time Complexity : O(N)
