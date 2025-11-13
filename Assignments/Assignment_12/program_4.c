a836ccf2fc8a3d6b5f4f9471711882bd6bc42897
Pradnyawani <pradnyawani7@gmail.com>
2025-11-13 12:08:45 +0530

    Add files via upload

---
+++ Assignments/Assignment_12/program_4.c
@@ -0,0 +1,47 @@
+
+
+
+#include<stdio.h>
+///////////////////////////////////////////////////////////////
+// Function Name : sum_natural_numbers
+// Description   : Accept number from user and return the sum 
+//                 of first N natural numbers.
+// Input         : Integer
+// Output        : Integer 
+// Author        : Pradnya Bhausaheb Wani
+// Date          :13/11/2025
+///////////////////////////////////////////////////////////////
+
+int sum_natural_numbers(int iCnt)
+{
+    int iNo = 0;
+    int iSum = 0;
+
+    if(iCnt <= 0)
+    {
+        printf("Enter number:\n");
+    }
+    for(iNo = 1; iNo <= iCnt; iNo++)
+    {
+        iSum = iSum + iNo;
+        
+    }
+    return iSum;
+
+        
+    }
+
+int main()
+{
+    int  limit = 0;
+    int iRet = 0;
+
+    printf("Enter Number:");
+    scanf("%d",&limit);
+
+   iRet = sum_natural_numbers(limit);
+   printf("Sum of natural %d number:%d\n", limit, iRet);
+
+    return 0;
+}
+// Time Complexity : O(N)
