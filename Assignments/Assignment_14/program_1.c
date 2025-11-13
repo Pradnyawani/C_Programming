8d4d7a7c1f536b33727f71c9c9f98573a9ea3b18
Pradnyawani <pradnyawani7@gmail.com>
2025-11-13 14:56:56 +0530

    Add files via upload

---
+++ Assignments/Assignment_14/program_1.c
@@ -0,0 +1,41 @@
+
+
+#include<stdio.h>
+///////////////////////////////////////////////////////////////
+// Function Name : DisplayDigit
+// Description   : Accept number from user and display its digits
+//                 in reverse order.
+// Input         : Integer
+// Output        : Digits in reverse order
+// Author        : Pradnya Bhausaheb Wani
+// Date          : 13/11/25
+///////////////////////////////////////////////////////////////
+
+void DisplayDigit(int iNo)
+{
+    int iDigit = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    while( iNo != 0)
+    {
+        iDigit= iNo % 10; // extract last digit
+        printf("%d\n",iDigit);
+        iNo = iNo/10; //remove last digit
+
+    }
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number:");
+    scanf("%d",&iValue);
+
+    DisplayDigit(iValue);
+
+    return 0;
+}
+// Time Complexity: O(n)
